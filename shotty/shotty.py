2e269b2f266fd9bf54347ee286dd32bb66500af2
Josh <wetzelquatl@gmail.com>
2019-09-25 18:20:59 -0700

    added try for starting or stopping while pending

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -125,20 +126,28 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
-
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}.".format(i.id))
+            continue
     return
 
 @instances.command('start')
 @click.option('--project', default=None,
     help='Only instances for project')
 def stop_instances(project):
-    "Stop EC2 instances"
+    "Start EC2 instances"
 
     instances=filter_instances(project)
 
     for i in instances:
         print("Starting {0}...".format(i.id))
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}.".format(i.id))
+            continue
         i.start()
 
     return
